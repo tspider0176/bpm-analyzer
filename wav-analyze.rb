71e2200cfc4f7f1409a3ba04b8f7d230bd95e4fa
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2016-11-14 02:31:40 +0900

    Remove some variable which is no longer in use

--- wav-analyze.rb
+++ wav-analyze.rb
@@ -2,48 +2,23 @@
 require 'rubygems'
 require 'wav-file'
 
-# wavファイルopen
-f = open("jump.wav")
+FRAME_LEN = 1024
+
+f = open("tempo_120.wav")
 format = WavFile::readFormat(f)
 data_chunk = WavFile::readDataChunk(f)
 f.close
 
-# サンプリング周期[s]
-t = 1.0/format.hz
-
-# バイナリからwavの波形を配列として取り出す
-bit =  if format.bitPerSample == 16 then # int16_t
-  's*'
-elsif format.bitPerSample == 8 then # signed char
-  'c*'
+def get_wav_array(data_chunk)
+  data_chunk.data.unpack(
+    if format.bitPerSample == 16 then
+      's*'
+    elsif format.bitPerSample == 8 then
+      'c*'
+    end
+  )
 end
-wavs = data_chunk.data.unpack(bit)
 
-# フレームの長さ(フレーム内のサンプル数)は1024とする
-FRAME_LEN = 1024
-
-# 剰余で余った部分は切り捨て
-sample_max =  wavs.size - wavs.size % FRAME_LEN
-
-# 解析対象のフレーム最大値
-frame_max = sample_max / FRAME_LEN
-
-# 対象の各フレーム内の音量をリストで取得
-# 512個の要素を含む配列の配列に変形して、最後の配列を除く(切り捨て)それぞれの配列について二条平均平方根を求める
-# これで0フレームからframe_maxフレームまでの音量が取得できる
-# 次に0番目と1番目の音量の差、2番目と3番目の音量の差、...と言った形で音量の差の配列を作成する
-# 音量の減少は考慮に入れない為、マイナス値は0とする
-diff_arr = wavs[0..sample_max]
-  .each_slice(FRAME_LEN) # wavファイルをFRAME_LENの集合に分解
-  .to_a # Enumerable -> Array
-  .map{|arr| Math.sqrt(arr.map{|elem| elem ** 2}.inject(:+) / arr.size)} # それぞれのフレームについて、二乗平均平方根を計算
-  .each_slice(2) # フレームのペアを作成
-  .to_a # Enumerable -> Array
-  .map{|x,y|
-    y != nil && x - y >= 0 ? x - y : 0 # 隣り合うフレーム同士の差を計算、ただしマイナス値は0とする
-  }
-
-# 渡されたdataとbpmについて、マッチ度の計算を行う
 def calc_bpm_match(data, bpm)
     f_bpm   = bpm / 60.0
     s = 44100.0 / FRAME_LEN
@@ -60,14 +35,26 @@ def calc_bpm_match(data, bpm)
     Math.sqrt(a_bpm ** 2 + b_bpm ** 2)
 end
 
-# 60から240までbpmのマッチ度を計算し、マッチ度のリストを返す
 def calc_match(data)
     (60..240).map{|bpm|
       calc_bpm_match(data, bpm)
     }
 end
 
-# TODO 別のマッチ方法の提案
+wavs = get_wav_array
+sample_max =  wavs.size - wavs.size % FRAME_LEN
+
+# 対象の各フレーム内の音量をリストで取得
+# FRAME_LEN個の要素を含む配列の配列に変形し、最後の配列を除く(切り捨て)それぞれの配列について二条平均平方根を求める
+# これで0フレームから最大フレームまでの音量が取得できる
+# 次に0番目と1番目の音量の差、2番目と3番目の音量の差、...と言った形で音量の差の配列を作成する
+# 音量の減少は考慮に入れない為、マイナス値は0とする
+diff_arr = wavs[0..sample_max]
+  .each_slice(FRAME_LEN).to_a
+  .map{|arr| Math.sqrt(arr.map{|elem| elem ** 2}.inject(:+) / arr.size)}
+  .each_slice(2).to_a
+  .map{|x,y| y != nil && x - y >= 0 ? x - y : 0}
+
 res = calc_match(diff_arr)
 puts "BPM,Match rate"
 res.each_with_index{|e, i| puts "#{i+60},#{e}"}
