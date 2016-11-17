0713bf60b605e130f42d99cf6febabebdf635053
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2016-11-17 20:43:04 +0900

    Refactoring

--- wav-analyze.rb
+++ wav-analyze.rb
@@ -3,11 +3,11 @@ require 'rubygems'
 require 'wav-file'
 require 'singleton'
 
+FRAME_LEN = 512
+SAMPLE_F_PER_FRAME = 44100.0 / FRAME_LEN
+
 class BPMAnalyzer
   include Singleton
-
-  @FRAME_LEN = 512
-  @spf = 44100.0 / @FRAME_LEN
 
   def initialize
     f = open("tempo_120.wav")
@@ -18,22 +18,18 @@ class BPMAnalyzer
   end
 
   def bit_per_sample(format)
-    if format.bitPerSample == 16 then
-      's*'
-    elsif format.bitPerSample == 8 then
-      'c*'
-    end
+    format.bitPerSample == 16 ? 's*' : 'c*'
   end
 
   def get_wav_array(data_chunk, format)
-    data_chunk.data.unpack(bit_per_sample(format))
+    data_chunk.data.unpack(bit_per_sample(format)) # chuck -> dataの配列へunpack
   end
 
   def calc_bpm_match(data, bpm)
       f_bpm   = bpm / 60.0
 
-      phase_cos = lambda{|m| Math.cos(2 * Math::PI * f_bpm * m / @spf)}
-      phase_sin = lambda{|m| Math.sin(2 * Math::PI * f_bpm * m / @spf)}
+      phase_cos = lambda{|m| Math.cos(2 * Math::PI * f_bpm * m / SAMPLE_F_PER_FRAME)}
+      phase_sin = lambda{|m| Math.sin(2 * Math::PI * f_bpm * m / SAMPLE_F_PER_FRAME)}
 
       a_bpm = (0..data.size-1).map{|m| phase_cos.(m)}.zip(data).map{|x,y| x * y}.inject(:+) / data.size
       b_bpm = (0..data.size-1).map{|m| phase_sin.(m)}.zip(data).map{|x,y| x * y}.inject(:+) / data.size
@@ -49,16 +45,16 @@ class BPMAnalyzer
 
   def run
     diff_arr = @wavs.take(@wavs.size - @wavs.size % FRAME_LEN)
-      .each_slice(FRAME_LEN).to_a
-      .map{|arr| Math.sqrt(arr.map{|elem| elem ** 2}.inject(:+) / arr.size)}
-      .each_slice(2).to_a
-      .map{|x,y| y != nil && x - y >= 0 ? x - y : 0}
+      .each_slice(FRAME_LEN).to_a # フレーム長毎に配列を細断
+      .map{|arr| Math.sqrt(arr.map{|elem| elem ** 2}.inject(:+) / arr.size)} # それぞれのフレームについて、二乗平均平方根を計算
+      .each_slice(2).to_a # 隣同士の配列でペア作成
+      .map{|x,y| y != nil && x - y >= 0 ? x - y : 0} # 差を計算、マイナス値は0へ
 
     @res = calc_match(diff_arr)
   end
 
   def to_s
-    "BPM,Match rate\n" + @res.each_with_index{|e, i| "#{i+60},#{e}"}.join("\n")
+    "BPM,Match rate\n" + @res.map.with_index{|e, i| "#{i+60},#{e}"}.join("\n") if @res != nil
   end
 end
 
